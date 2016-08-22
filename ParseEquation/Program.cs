0ba59b877534e93efeaaaf3f15779e0d70220308
James R Morey <jamesmorey316@gmail.com>
2016-08-22 12:59:15 -0700

    50% done with initial methods.

--- ParseEquation/Program.cs
+++ ParseEquation/Program.cs
@@ -18,7 +18,7 @@ namespace ComputeEquation
             string eqBad2 = "17x^4y^3 * -9x^3y^2 + 87yx^2 - 19x";
 
 
-            Console.WriteLine(Calculate.ParseEquation(eqBad2));
+            Console.WriteLine(Calculate.IsValidEquation(eqBad2));
 
             Console.ReadKey();
 
@@ -27,6 +27,8 @@ namespace ComputeEquation
 
     public static class Calculate
     {
+
+        #region Simple Polynomial Method
         /// <summary>
         /// Method: Polynomial - This is the public-facing method that takes a string (the equation) + 
         ///     a decimal[] the decimal values of X, Y, and Z and returns the decimal answer of the equation
@@ -43,20 +45,20 @@ namespace ComputeEquation
         /// 
         ///  Exception handling is done in helper methods, because they have the proper context 
         ///  to provide more details to consumers.</remarks>
-        public static decimal Polynomial(string eq, decimal[] values)
+        public static decimal SimplePolynomial(string eq, decimal[] values)
         {
             decimal ret = 0;
             Dictionary<int, string> ops = new Dictionary<int, string>();
             List<string> substr = new List<string>();
-            List<SubEquation> subs = new List<SubEquation>();
+            List<Term> terms = new List<Term>();
 
             //make sure equation string follows the rules first
-            if (ParseEquation(eq))
+            if (IsValidEquation(eq))
             {
                 ops = FindOperators(eq);
-                substr = GetSubEquationStrings(eq, ops);
-                subs = ParseSubEquation(substr, values);
-                ret = FinalCalc(ops, subs);
+                substr = FindTerms(eq, ops);
+                terms = ParseTerms(substr);
+                ret = FinalCalc(ops, terms);
             }
 
 
@@ -75,11 +77,12 @@ namespace ComputeEquation
         /// <param name="eq">String - The equation to be parsed.</param>
         /// <returns>Boolean - True if the given equation follows the rules or False if not</returns>
         /// <exception cref="">"The equation was not properly formed. Please check the equation and try again."</exception>
-        public static bool ParseEquation(string eq)
+        public static bool IsValidEquation(string eq)
         {
             // Regex checks for any 'illegal' characters. No point in continuing if 
             //  if consumer provides bad equation. 
             Regex regx = new Regex(@"[0-9]\s\^\-\+xyz", RegexOptions.IgnoreCase);
+
 
             if (regx.IsMatch(eq))
                 return true;
@@ -103,8 +106,8 @@ namespace ComputeEquation
             // Iterate through equation string and find all the legal operators
             for (int i = 0; i <= eq.Length; i++)
             {
-                // Using switch in case (no pun intended) I want to add more legal operators in future releases
-                //  If we find a match add the location in the string and the operator. 
+                // Using switch in case (no pun intended) I want to add more legal operators in future releases.
+                // If we find a match add the location in the string and the operator. 
                 switch (eq.Substring(i,1))
                 {
                    case "-":
@@ -129,20 +132,22 @@ namespace ComputeEquation
         /// </summary>
         /// <param name="eq">String - The equation to be parsed.</param>
         /// <returns>List(string) - The terms in the equation as strings.</returns>
-        public static List<string> GetSubEquationStrings(string eq, Dictionary<int, string> ops)
+        public static List<string> FindTerms(string eq, Dictionary<int, string> ops)
         {
-            List<string> subs = new List<string>();
+            List<string> terms = new List<string>();
             string eqCopy = eq;
-            // Make sure that the term matches Int[xyz]^Int[xyz]^Int[xyz]^Int
-            Regex regx = new Regex(@"^[0-9][xyz]\^[0-9]*[xyz]\^[0-9]*[xyz]\^[0-9]*", RegexOptions.IgnoreCase);
+            // Make sure that the term matches a valid polynomial term (e.g. -17x^5y^4x^3)
+            Regex regx = new Regex(@"^\-?([0-9]+)(x\^[0-9]+)?(y\^[0-9]+)?(z\^[0-9]+)?", RegexOptions.IgnoreCase);
 
 
+            // Iterate through the equation string (positioned by the operator), grab the substring that represents the 
+            //  subequation, validate it, and if valid, add it to the List(string) that represent the equation terms 
             foreach (KeyValuePair<int, string> kp in ops)
             {
                 string sub = eqCopy.Substring(0, kp.Key);
                 if (regx.IsMatch(sub))
                 {
-                    subs.Add(sub);
+                    terms.Add(sub);
                     eqCopy = eqCopy.Substring(kp.Key, eqCopy.Length - kp.Key);
                 }
                 else
@@ -150,8 +155,74 @@ namespace ComputeEquation
                     throw new Exception($"The equation term {sub} was not properly formed. Check the equations and try again.");
                 }
             }
-            return subs;
+            return terms;
         }
+
+
+        /// <summary>
+        /// Private Method FindExponent - Parses input string (term) and finds the x, y, and z exponents (if any).
+        /// </summary>
+        /// <param name="term">String representing the equation term.</param>
+        /// <returns>Int[] - int array representing the exponents of the x,y, and z variables.</returns>
+        /// <remarks>If the variable has no exponent, FindExponent returns 1 so that N = N.</remarks>
+        public static int[] FindExponent(string term)
+        {
+            int[] exp = { 1, 1, 1}; // By default return N^1 so that N = N if there is no exponent.
+            int pos = 0;
+            int parseTry = 0;
+            string expStr = "";
+
+            // Get position of x exponent.
+            pos = term.IndexOf('x') + 2;
+            for (int i = 0; i < term.Length - pos; i++)
+            {
+                int.TryParse( term.Substring(pos,1), out parseTry);
+                if (parseTry > 0)
+                    expStr += term.Substring(pos, 1);
+                else
+                    break;
+
+            }
+            int.TryParse(expStr, out parseTry);
+            if (parseTry > 0)
+                exp[0] = parseTry;
+
+
+            // Get position of y exponent.
+            pos = term.IndexOf('y') + 2;
+            for (int i = 0; i < term.Length - pos; i++)
+            {
+                int.TryParse(term.Substring(pos, 1), out parseTry);
+                if (parseTry > 0)
+                    expStr += term.Substring(pos, 1);
+                else
+                    break;
+
+            }
+            int.TryParse(expStr, out parseTry);
+            if (parseTry > 0)
+                exp[1] = parseTry;
+
+
+            // Get position of z exponent.
+            pos = term.IndexOf('z') + 2;
+            for (int i = 0; i < term.Length - pos; i++)
+            {
+                int.TryParse(term.Substring(pos, 1), out parseTry);
+                if (parseTry > 0)
+                    expStr += term.Substring(pos, 1);
+                else
+                    break;
+
+            }
+            int.TryParse(expStr, out parseTry);
+            if (parseTry > 0)
+                exp[1] = parseTry;
+
+
+            return exp;
+        }
+
 
 
         /// <summary>
@@ -163,18 +234,48 @@ namespace ComputeEquation
         /// <returns>List(SubEquation) - A list of all the equations terms as SubEquation objects.</returns>
         /// <remarks>ParseSubEquation() receives its input from GetSubEquationStrings()</remarks>
         /// <exception cref="">"One or more equation terms was not properly formed. Check the equations and try again."</exception>
-        public static List<SubEquation> ParseSubEquation(List<string> subeq, decimal[] values)
+        public static List<Term> ParseTerms(List<string> subeq)
         {
-            List<SubEquation> subs = new List<SubEquation>();
+            List<Term> subs = new List<Term>();
 
 
             foreach (var sub in subeq)
             {
+                int len = sub.Length - 1;   // The standard basic guard against OutOfRange exception ;- )
+                int coef = 0;   // This is the actual value of the coefficient. This value will be stuffed into the SubEquation.Coefficient property.
+                bool hasOp = false; // If true then the string representing the coefficient will have '-' at the beginning.
+                int isInt = 0;  // Used as out int of the TryPare(). IF it is not 0 then it represents the parsed int value.
+                int position = 0;   // position determines the parsing starts. IF there is a sign on the coefficient then it bumps position 1.
+
+                // Detect the sign of the coefficient. "+" should never be input, but check for it anyway.
+                if(sub.Substring(position,1) == "-")
+                {
+                    hasOp = true;
+                    position = 1;
+                }
+                else if (sub.Substring(position, 1) == "+")
+                {
+                    position = 1;
+                }
+
+
+
+                // Parse through the remainder of the subequation string and create the SubEquation class instance and then add it to the List(SubEquations)
+                for (int i = position; i <= len; i++)
+                {
+                    
+
+
+
+
+
+                }
 
             }
 
             return subs;
         }
+
 
 
         /// <summary>
@@ -183,32 +284,55 @@ namespace ComputeEquation
         /// <param name="operators">Dictionary(int,</param>
         /// <param name="subs"></param>
         /// <returns></returns>
-        public static decimal FinalCalc(Dictionary<int, string> operators, List<SubEquation> subs)
+        public static decimal FinalCalc(Dictionary<int, string> operators, List<Term> terms)
         {
             decimal final = 0;
 
             return final;
         }
 
-
+        #endregion
     }
 
 
     /// <summary>
     /// Public Class SubEquation - This class is used to represent the terms in the equation in a way that computer-based calculation can be done.
     /// </summary>
-    public class SubEquation
+    public class Term
     {
-        public SubEquation() { }
+        public Term()
+        {
+            xPower = 1;
+            yPower = 1;
+            zPower = 1;
+        }
 
 
         public int Coefficient { get; set; }
 
-        public int TermX { get; set; }
+        public int xPower { get; set; }
 
-        public int TermY { get; set; }
+        public int yPower { get; set; }
 
-        public int TermZ { get; set; }
+        public int zPower { get; set; }
+
+    }
+
+    /// <summary>
+    /// Public class Equation - Class representing the input equation, assuming it is valid. The purpose of this is to be able serialize the valid 
+    ///     equations to a DB so that, if the equation is needed again, it can quickly be deserialized.
+    /// </summary>
+    public class Euqation
+    {
+        public Euqation() { }
+
+        public string EquationString { get; set; }
+
+        public List<Term> Terms { get; set; }
+
+        public Dictionary<int,string> Operators { get; set; }
+
+        public string Hash { get; set; }
 
     }
 }
---
+++ UnitTestParseEquation/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ComputeEquation;
+
+namespace UnitTestParseEquation
+{
+    [TestClass]
+    public class TestSimplePolynomial
+    {
+        [TestMethod]
+        public void TestFindOperators()
+        {
+
+
+        }
+    }
+}
