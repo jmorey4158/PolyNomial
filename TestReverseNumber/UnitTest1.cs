ee6f8964c3ce3fed6382f04d8087de7d90edb467
James R Morey <jamesmorey316@gmail.com>
2016-08-23 15:01:08 -0700

    Added Unit Tests

---
+++ TestReverseNumber/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReverseNumber;
+
+namespace TestReverseNumber
+{
+    [TestClass]
+    public class TestReverseNumber
+    {
+        [TestMethod]
+        public void ReverseInt_GoodPositiveInt_ShouldPass()
+        {
+
+            int input1 = 123456789;
+            int output1 = 987654321;
+
+            Assert.AreEqual(output1, Operation.Reverse(input1));
+        }
+
+        [TestMethod]
+        public void ReverseInt_GoodNegativeInt_ShouldPass()
+        {
+
+            int input1 = -123456789;
+            int output1 = -987654321;
+
+            Assert.AreEqual(output1, Operation.Reverse(input1));
+        }
+
+
+
+        [TestMethod]
+        public void ReverseLng_GoodPositiveLong_ShouldPass()
+        {
+
+            long input1 = 123456789012345678;
+            long output1 = 876543210987654321;
+
+            Assert.AreEqual(output1, Operation.Reverse(input1));
+        }
+
+        [TestMethod]
+        public void ReverseLong_GoodNegativeLong_ShouldPass()
+        {
+
+            long input1 = -123456789123456789;
+            long output1 = -987654321987654321;
+
+            Assert.AreEqual(output1, Operation.Reverse(input1));
+        }
+
+
+    }
+}
