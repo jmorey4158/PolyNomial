fae7745ee1acf448e61d09572469378c898cea55
James R Morey <jamesmorey316@gmail.com>
2016-08-27 19:52:08 -0700

    cleaning up

--- AdHocTests/Program.cs
+++ AdHocTests/Program.cs
@@ -20,45 +20,6 @@ namespace AdHocTests
             Console.ReadKey();
         }
 
-
-        public static bool CompareDictionary(Dictionary<int, string> pattern, Dictionary<int, string> test)
-        {
-            
-            int[] pkeys = new int[pattern.Count];
-            string[] pvals = new string[pattern.Count];
-
-            int[] tkeys = new int[test.Count];
-            string[] tvals = new string[test.Count];
-
-
-            int i = 0;
-            foreach (KeyValuePair<int,string> kp in pattern)
-            {
-                pkeys[i] = kp.Key;
-                pvals[i] = kp.Value;
-                i++;
-            }
-            i = 0;
-            foreach (KeyValuePair<int,string> kp in test)
-            {
-                tkeys[i] = kp.Key;
-                tvals[i] = kp.Value;
-                i++;
-            }
-
-            for (int k = 0; k < pkeys.Length; k++)
-            {
-                if (pkeys[k] != tkeys[k])
-                    return false;
-
-                if (pvals[k] != tvals[k])
-                    return false;
-            }
-
-            return true;
-        }
-
-
     }
 
 
